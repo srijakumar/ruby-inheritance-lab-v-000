2097f005fb30981578118ee4186a14e0904f314b
Srija S <srijakumar7@gmail.com>
2018-05-21 01:46:31 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,14 +2,11 @@ class User
 
   attr_accessor :first_name, :last_name
 
-  @@KNOWLEDGE = []
 
   def initialize
     @first_name = first_name
     @last_name = last_name
   end
 
-  def teach
-      KNOWLEDGE.sample
-  end
+
 end
